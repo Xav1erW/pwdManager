8e62a11f482e589dcc5798feccf90d7891e73c87
Xav1erW <xavierwang035@outlook.com>
2022-05-14 12:02:41 +0800

    主页选择collection和password

--- pwdManager/frontend/src/pages/Main/Main.tsx
+++ pwdManager/frontend/src/pages/Main/Main.tsx
@@ -1,12 +1,121 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+import classNames from 'classnames/bind'
+import api from 'src/utils/api'
 import Topbar from 'src/Components/Topbar/Topbar'
 import styles from './styles/Main.module.scss'
+
+interface collectionItemTyle {
+    name: string,
+    uuid: string
+}
+
+interface passwordItemTyle {
+    name: string,
+    uuid: string
+}
+
+interface pwdInfo {
+    title: string,
+    username: string,
+    password: string,
+    url: string,
+    description: string
+}
+
+interface pwdDetailsInfo {
+    title: string,
+    username: string,
+    password: string,
+    url: string,
+    description: string,
+
+    updateDate: string,
+    createTime: number, // timestamp
+    updateTime: number, // timestamp
+    updateHistory: string[],
+    autoComplete: Boolean,
+    matchRules: string[], //url list
+
+}
+
+interface collectionListTyle extends Array<collectionItemTyle> { }
+interface passwordListTyle extends Array<passwordItemTyle> { }
 export default function Main() {
+    const [collectionList, setCollectionList] = useState<collectionListTyle>([])
+    const [passwordList, setPasswordList] = useState<passwordListTyle>([])
+    const [pwdInfo, setPwdInfo] = useState<pwdInfo|pwdDetailsInfo>({} as pwdInfo)
+    const [chosenCollection, setChosenCollection] = useState('')
+    const [chosenPassword, setChosenPassword] = useState('')
+    classNames.bind(styles)
+    const collectionNavClass = classNames({
+        nav: true,
+        collectionNav: true,
+    })
+    const passwordNavClass = classNames({
+        nav: true,
+        passwordNav: true,
+    })
+    const actCollectionItemClass = classNames({
+        navItem: true,
+        activateItem: true,
+        collection: true
+    })
+    const collectionItemClass = classNames({
+        navItem: true,
+    })
+    const actPasswordItemClass = classNames({
+        navItem: true,
+        activateItem: true,
+        password: true
+    })
+    const passwordItemClass = classNames({
+        navItem: true,
+    })
+    useEffect(() => {
+        api.getCollectionList().then(res => {
+            setCollectionList(res.collectionList)
+        })
+    }, [])
+
+    const chooseCollection = (collectionUUID: string) => {
+        return ()=>{
+            setChosenCollection(collectionUUID)
+            api.getPasswordList(collectionUUID).then(res => {
+                setPasswordList(res.passwordList)
+            })
+        }
+    }
+    const choosePassword = (passwordUUID: string) => {
+        return ()=>{
+            setChosenPassword(passwordUUID)
+            api.getPasswordInfo(passwordUUID).then(res => {
+                setPwdInfo(res)
+            })
+        }
+    }
     return (
         <div className={styles.mainPage}>
-            <Topbar/>
-            
+            <Topbar />
+            <div className={styles.mainContent}>
+                <div className={collectionNavClass}>
+                    {collectionList.map(item =>
+                    (
+                        <div className={chosenCollection===item.uuid?actCollectionItemClass:collectionItemClass} key={item.uuid} onClick={chooseCollection(item.uuid)}>
+                            <span>{item.name}</span>
+                            <hr/>
+                        </div>
+                    ))}
+                </div>
+                {passwordList?<div className={passwordNavClass}>
+                    {passwordList.map(item =>(
+                        <div className={chosenPassword===item.uuid?actPasswordItemClass:passwordItemClass} key={item.uuid}>
+                            <span>{item.name}</span>
+                            <hr/>
+                        </div>
+                    ))}
+                </div>:null}
+            </div>
         </div>
     )
 }
--- pwdManager/frontend/src/utils/api.ts
+++ pwdManager/frontend/src/utils/api.ts
@@ -1,6 +1,46 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
 
-import { encrypt } from "./rsa";
+import { encrypt, decrypt } from "./rsa";
+
+interface CollectionResponse {
+    collectionList: {
+        name: string,
+        uuid: string
+    }[],
+    dbName: string
+}
+
+interface pwdListResponse {
+    passwordList: {
+        name: string,
+        uuid: string
+    }[],
+}
+
+interface pwdResponse {
+    title: string,
+    username: string,
+    password: string,
+    url: string,
+    description: string
+}
+
+interface pwdDetailsResponse {
+    title: string,
+    username: string,
+    password: string,
+    url: string,
+    description: string,
+
+    updateDate: string,
+    createTime: number, // timestamp
+    updateTime: number, // timestamp
+    updateHistory: string[],
+    autoComplete: Boolean,
+    matchRules: string[], //url list
+
+}
+
 export class Api {
     authToken: string = "";
     baseUrl: string = "/api/";
@@ -9,23 +49,23 @@ export class Api {
     private serverPublicKey: string = ""
     constructor(baseUrl?: string) {
         this.client = axios.create({
-            baseURL: baseUrl?baseUrl:this.baseUrl,
+            baseURL: baseUrl ? baseUrl : this.baseUrl,
             headers: {
                 'Authorization': this.authToken
             }
         })
     }
 
-    async get(url: string):Promise<any> {
+    async get(url: string): Promise<any> {
         const response = await this.client.get(url)
         if (response.status === 200) {
             return response
-        } else if(response.status === 401) {
+        } else if (response.status === 401) {
             throw new Error("Unauthorized")
         }
     }
 
-    async post(url: string, data: any):Promise<any> {
+    async post(url: string, data: any): Promise<any> {
         const response = await this.client.post(url, data)
         if (response.status === 200) {
             return response
@@ -34,7 +74,7 @@ export class Api {
         }
     }
 
-    async BeforeLogin(uuid: string, pubKey: string):Promise<any> {
+    async BeforeLogin(uuid: string, pubKey: string): Promise<any> {
         const response = await this.post('auth', {
             'session id': uuid,
             'public key': pubKey
@@ -42,24 +82,93 @@ export class Api {
         const jwt = response.data['jwt']
         const publicKey = response.data["public key"]
         this.setServerPublicKey(publicKey)
-        return {jwt, publicKey}
+        return { jwt, publicKey }
     }
 
-    async getFileList():Promise<any> {
+    async getFileList(): Promise<any> {
         const response = await this.get('fileList')
         return response.data
     }
 
-    async Login(dbUUID: string, password: string):Promise<any> {
+    async Login(dbUUID: string, password: string): Promise<any> {
         console.log('password', password)
         console.log('key', this.serverPublicKey)
         const encryptedPassword = encrypt(password, this.serverPublicKey)
-        const response = await this.post(`verify?uuid=${dbUUID}`, {password: encryptedPassword})
+        const response = await this.post(`verify?uuid=${dbUUID}`, { password: encryptedPassword })
         if (response.status === 200) {
             this.authToken = response.data['jwt']
+            // 添加dbUUID到header中作为参数
+            this.client.defaults.headers.common['dbUUID'] = dbUUID
             return response.data
-        }else{
+        } else {
             throw new Error(response.data['msg'])
+        }
+    }
+
+
+    async getCollectionList(): Promise<CollectionResponse> {
+        const response = await this.get('collection/list')
+        if (response.status === 200) {
+            const data: CollectionResponse = { collectionList: response.data.data, dbName: response.data.dbName }
+            return data
+        }
+        else {
+            throw new Error(response.status)
+        }
+    }
+
+    async getPasswordList(collectionUUID: string): Promise<pwdListResponse> {
+        const response = await this.get(`collection/info?collectionID=${collectionUUID}`)
+        if (response.status === 200) {
+            const data: pwdListResponse = { passwordList: response.data.data }
+            return data
+        }
+        else {
+            throw new Error(response.status)
+        }
+    }
+
+    async getPasswordInfo(passwordUUID: string, detail: Boolean = false): Promise<pwdResponse | pwdDetailsResponse> {
+        const response = await this.get(`password/info?passwordID=${passwordUUID}&detail=${detail}`)
+        if (detail) {
+            if (response.status === 200) {
+                const username = decrypt(response.data.username, this.privateKey)
+                const password = decrypt(response.data.password, this.privateKey)
+                const updateHistory = response.data.updateHistory.map((item:string) => decrypt(item, this.privateKey))
+                const data: pwdDetailsResponse = {
+                    title: response.data.title,
+                    username: username,
+                    password: password,
+                    url: response.data.url,
+                    description: response.data.description,
+                    updateDate: response.data.updateDate,
+                    createTime: response.data.createTime,
+                    updateTime: response.data.updateTime,
+                    updateHistory: updateHistory,
+                    autoComplete: response.data.autoComplete,
+                    matchRules: response.data.matchRules,
+                }
+                return data
+            }
+            else{
+                throw new Error(response.status)
+            }
+        } else {
+            if (response.status === 200) {
+                const username = decrypt(response.data.username, this.privateKey)
+                const password = decrypt(response.data.password, this.privateKey)
+                const data: pwdResponse = {
+                    title: response.data.title,
+                    username: username,
+                    password: password,
+                    url: response.data.url,
+                    description: response.data.description
+                }
+                return data
+            }
+            else {
+                throw new Error(response.status)
+            }
         }
     }
 
